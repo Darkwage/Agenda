547db57355a6e0b466dfab2765604ede8a5a744b
Darkwage <darkwage0@gmail.com>
2026-09-12 17:09:15 +0200

    Register local plugins in MainActivity onCreate
    Override onCreate method to register local plugins.

--- android/app/src/main/java/com/arwin/agenda/MainActivity.java
+++ android/app/src/main/java/com/arwin/agenda/MainActivity.java
@@ -1,5 +1,14 @@
 package com.arwin.agenda;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Local (non-npm) plugins must be registered manually, before the
+        // bridge finishes initializing in super.onCreate().
+        registerPlugin(WidgetBridgePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
